5ace4877f36a0553c2a23d5811b3945db0941cee
yuhao.wang3 <yuhao.wang3@56qq.com>
2020-05-28 14:30:12 +0800

    初始化

---
+++ provider-demo/src/test/java/com/xiaolyuh/tsf/provider/ProviderDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaolyuh.tsf.provider;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
