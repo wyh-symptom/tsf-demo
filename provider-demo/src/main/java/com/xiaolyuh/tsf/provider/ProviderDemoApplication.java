662e5a927bef08468d88e53f543e63abd715fc29
yuhao.wang3 <yuhao.wang3@56qq.com>
2020-05-28 16:51:09 +0800

    生产者

--- provider-demo/src/main/java/com/xiaolyuh/tsf/provider/ProviderDemoApplication.java
+++ provider-demo/src/main/java/com/xiaolyuh/tsf/provider/ProviderDemoApplication.java
@@ -2,10 +2,12 @@ package com.xiaolyuh.tsf.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.tsf.annotation.EnableTsf;
 
 @SpringBootApplication
-//@EnableFeignClients // 使用Feign微服务调用时请启用
-//@EnableTsf
+@EnableFeignClients // 使用Feign微服务调用时请启用
+@EnableTsf
 public class ProviderDemoApplication {
 
     public static void main(String[] args) {
